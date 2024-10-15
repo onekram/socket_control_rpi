fc8f3c479bc62b51de5e1a66caa13c017b8eb57c
Vyasanklav <124244545+Vyasanklav@users.noreply.github.com>
2024-10-15 17:26:28 +0400

    hand.py

--- hand.py
+++ hand.py
@@ -10,7 +10,7 @@ def start_position(s: socket.socket):
     move_servo(s, ServoKind.SHOULDER, 170)
     move_servo(s, ServoKind.ELBOW, 100)
 
-def catch(s: socket.socket):
+def catch(s: socket.socket): # приготовится к захвату
     move_servo(s, ServoKind.GRAB, 20)
     time.sleep(0.3)
     move_servo(s, ServoKind.HAND, 95)
@@ -24,14 +24,14 @@ def catch_2(s: socket.socket):
     move_servo(s, ServoKind.SHOULDER, 80)
     move_servo(s, ServoKind.ELBOW, 180)
 
-def hold(s: socket.socket):
+def hold(s: socket.socket): # захватить и поднять
     move_servo(s, ServoKind.GRAB, 70)
     time.sleep(0.5)
     move_servo(s, ServoKind.HAND, 10)
     move_servo(s, ServoKind.SHOULDER, 170)
     move_servo(s, ServoKind.ELBOW, 100)
 
-def put_down(s: socket.socket):
+def put_down(s: socket.socket): # отпустить объект
     move_servo(s, ServoKind.HAND, 95)
     time.sleep(0.5)
     move_servo(s, ServoKind.GRAB, 10)
