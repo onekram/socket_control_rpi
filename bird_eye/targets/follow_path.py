95ec624c18923908af704a9fc5b633a2da415c11
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-10-18 16:09:07 +0300

    feat: Add moving and rotating

--- bird_eye/targets/follow_path.py
+++ bird_eye/targets/follow_path.py
@@ -1,9 +1,16 @@
-from typing import Tuple
-import movement
 import functions
+import socket
 import numpy as np
+from typing import Tuple
+from movement import turn_to_left, turn_to_right
 
-    
+
+def rotate_by_angle(s: socket.socket, angle : float) -> None:
+    if angle < 0:
+        turn_to_right(socket, abs(angle)) #????
+    else:
+        turn_to_left(socket, abs(angle)) #????
+
 
 def angle_between_vectors(point_start : Tuple[float, float], point_end_1 : Tuple[float, float], point_end_2 :  Tuple[float, float]) -> float:
     vector_1 : Tuple[float, float] = (point_end_1[0] - point_start[0], point_end_1[1] - point_start[1])
@@ -14,11 +21,8 @@ def angle_between_vectors(point_start : Tuple[float, float], point_end_1 : Tuple
 
     return angle1 - angle2
 
-def robot_to_point(robot_coords: Tuple[float, float], grab_coords: Tuple[float, float], point_coords: Tuple[float, float]):
+def robot_to_point(s: socket.socket, robot_coords: Tuple[float, float], grab_coords: Tuple[float, float], point_coords: Tuple[float, float]):
     dist = np.linalg.norm(point_coords - robot_coords)
     angle = angle_between_vectors(robot_coords, grab_coords, point_coords)
     rotate_by_angle(angle)
-    go_forward_by_dist(dist)
-
-
-print(angle_between_vectors((0, 0), (1, 15), (3, 12)))
+    #go_forward_by_dist(dist)
