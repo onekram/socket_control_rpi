d0f7b08e03149cde7755295782e77b3dc43db8db
Ivan Glazov <lolik1920@yandex.ru>
2024-10-21 20:45:03 +0500

    Feat: Define cube and walls position

--- script.py
+++ script.py
@@ -1,14 +1,25 @@
 from bird_eye.walls.model import WallsModel
+from bird_eye.walls.parse_objects import robot_position, get_corners
 import cv2
 from frame import get_frame
 from model import Model
 
 
-# h - 0, v - 1
-def h_or_v_walls(model_walls: WallsModel, frame) -> bool:
-    objs = model_walls.get_objs(frame)
+# 0 - h, 1 - v
+def h_or_v_walls(objs) -> int:
     return "part_up" in objs
 
+# 0 - outside, 1 - inside bot, 2 - inside top
+def cube_pos(objs, mp) -> int:
+    x, y, _, _ = mp["cube"][0].xywh[0]
+    obj_pos = robot_position((x, y), objs["wall1"], objs["wall2"], objs["wall3"])
+
+    if obj_pos == 1:
+        return 0
+    elif y < 447:
+        return 1
+    else:
+        return 2
 
 def parse_generation() -> list:
     gen = []
@@ -18,21 +29,15 @@ def parse_generation() -> list:
     frame_ = get_frame(cap)
 
     model_walls_ = WallsModel('bird_eye/walls/walls.onnx')
+    wall_objs = model_walls_.get_objs(frame_)
     model_bird_eye = Model('bird_eye/targets/bird_eye_best_v3.onnx')
 
-    map = model_bird_eye.get_map(frame_)
+    mp = model_bird_eye.get_map(frame_)
 
-    # for name, value in map.items():
-    #     print('------------------------------------------------')
-    #     print(name)
-    #     print(value)
-    #     print('------------------------------------------------')
-    # gen.append(h_or_v_walls(model_walls_, frame_))
+    gen.append(h_or_v_walls(wall_objs))
+    gen.append(cube_pos(wall_objs, mp))
 
-    for cube in map["cube"]:
-        x, y, _, _ = cube.xywh[0]
-        x, y = int(x), int(y)
-        print(x, y)
+    return gen
 
 if __name__ == "__main__":
     parse_generation()
